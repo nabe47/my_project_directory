1a8b659a33ab5bd84267d9bca4eeff785c387765
nabe47 <nambiprofessionnel@gmail.com>
2023-05-21 11:48:34 -0800

    CRUD Ingrédient terminé

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -7,7 +7,6 @@ use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,7 +48,7 @@ class IngredientController extends AbstractController
         EntityManagerInterface $manager /* Le manager qui va executer la requête du CREATE*/
         ):Response
     {
-        $ingredient = new Ingredient(); /** Créer un nouveau Ingrédient pour éviter les erreurs liées à la portée des variables */
+        $ingredient = new Ingredient(); /** Créer un nouveau Ingrédient vide pour éviter les erreurs liées à la portée des variables */
         $form = $this->createForm(IngredientType::class, $ingredient);/** Enregister dans la variable $form les informations du formulaire BASES sur l'entité Ingrédient*/
 
         $form->handleRequest($request); /* Soumettre les données au formulaire */
@@ -60,7 +59,7 @@ class IngredientController extends AbstractController
             $manager->flush();/* Envoyer dans la base de données */
             $this->addFlash( /** Le petit message flash comme dans AUGURE pour dire que c'est enregistrer*/
                 'success',/** Le nom du message pour être récupérer dans le twig */
-                'Votre ingrédient a été créé avec succès' /**Le contenu du message */
+                'Votre ingrédient a été modifié avec succès' /**Le contenu du message */
             );
             return $this->redirectToRoute('ingredient.index'); /**Atteindre la vue qui à pour nom ingredient.index  ici c'est l'affichage des données dans la table*/
             #dd($ingredient); /* Pour afficher les données contenu dans $ingredient, méthode de vérification avant de flush dans la base*/
@@ -69,5 +68,75 @@ class IngredientController extends AbstractController
         return $this->render('pages/ingredient/new.html.twig', /**Atteindre la VUE (la page) lorsque la route est exacte */ [
             'form' => $form->createView() /** Envoyer une vue de formulaire basé sur les information de form/IngredienType */
         ]);
+    }
+
+    #[Route('ingredient/edition/{id}', name:'ingredient.edit',methods:['GET','POST'])]
+    public function edit(
+        Ingredient $ingredient,
+        Request $request,
+        EntityManagerInterface $manager
+    ):Response
+    {
+        /**
+         * Petit Résumer de ce que fait controlleur:
+         * 1- Si la lien dans la barre d'adresse correspond à la route executer le controleur
+         * 2- Enregistrer dans un form l'ingrédient avec l'id spécifié (explication en bas)
+         * 3- Normalement le formulaire est déjà rempli par rapport à l'ingrédient spécifié
+         * 4- Soumettre les données du formulaire déjà rempli
+         * 5- Vérifier si les données sont ok
+         * 6- Recevoir les données du formulaire modifiées
+         * 7- Préparer à envoyer le nouvel ingrédient dans la base de données
+         * 8- Envoyer dans la base de données
+         */
+        
+        /**Explication de la magie
+          *Symfony passe directement l'ingrédient comme paramètre, 
+          *Symfony verifie si l'entité ingrédient possède un élément id (comme préciser dans la route)
+          *Puis passe directement l'ingrédient (id, name, price, createdAt) dans la vue  
+          *Le reste se passe comme dans new ingredient
+          */
+          $form = $this->createForm(IngredientType::class, $ingredient);
+          $form->handleRequest($request); /* Soumettre les données au formulaire */
+
+          if($form->isSubmitted() && $form->isValid()){ /* Si envoie du formulaire, vérifier si les données sont valides */
+            $ingredient = $form->getData();/* Recevoir les données du formulaire et stocker dans $ingredient*/
+            $manager->persist($ingredient);/* Préparer à evoyer dans la bdd les informations dans $ingredient*/
+            $manager->flush();/* Envoyer dans la base de données */
+            $this->addFlash( /** Le petit message flash comme dans AUGURE pour dire que c'est enregistrer*/
+                'success',/** Le nom du message pour être récupérer dans le twig */
+                'Votre ingrédient a été créé avec succès' /**Le contenu du message */
+            );
+            return $this->redirectToRoute('ingredient.index'); /**Atteindre la vue qui à pour nom ingredient.index  ici c'est l'affichage des données dans la table*/
+            }
+        
+        return $this->render('pages/ingredient/edit.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/ingredient/suppression/{id}',name:'ingredient.delete',methods:['GET','POST'])]
+    public function delete(
+        EntityManagerInterface $manager,
+        Ingredient $ingredient
+        ):Response
+    {
+        if(!$ingredient ){/**Si l'ingrédient n'existe pas */
+            $this->addFlash(
+                'error',
+                'Ingrédient non trouvé' /**Le contenu du message */
+            );
+            return $this->redirectToRoute('ingredient.index');
+        }
+        $manager->remove($ingredient);
+        $manager->flush();
+
+        $this->addFlash( /** Le petit message flash comme dans AUGURE pour dire que c'est enregistrer*/
+            'success',/** Le nom du message pour être récupérer dans le twig */
+            'Votre ingrédient a été supprimé avec succès' /**Le contenu du message */
+        );
+        return $this->redirectToRoute('ingredient.index'); /**Atteindre la vue qui à pour nom ingredient.index  ici c'est l'affichage des données dans la table*/
+        
+        return 1;
+
     }
 }
